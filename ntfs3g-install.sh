c8cce2915554763408d2589a13a575260203a5dd
Pavel R <ryabkov.p@gmail.com>
2019-10-22 08:26:54 +0300

    Add files via upload

---
+++ ntfs3g-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dnf -y groupinstall "Development Tools"
+dnf -y install tar
+
+curl https://tuxera.com/opensource/ntfs-3g_ntfsprogs-2017.3.23.tgz | tar -xvpz
+
+cd ntfs-3g_ntfsprogs-2017.3.23 && ./configure --prefix=/usr/local --disable-static
+
