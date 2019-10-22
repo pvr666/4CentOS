c4c984291fbdd0150e6bbd5082adb033c9da33d1
Pavel R <ryabkov.p@gmail.com>
2019-10-22 08:30:46 +0300

    Update ntfs3g-install.sh

--- ntfs3g-install.sh
+++ ntfs3g-install.sh
@@ -5,5 +5,4 @@ dnf -y install tar
 
 curl https://tuxera.com/opensource/ntfs-3g_ntfsprogs-2017.3.23.tgz | tar -xvpz
 
-cd ntfs-3g_ntfsprogs-2017.3.23 && ./configure --prefix=/usr/local --disable-static
-
+cd ntfs-3g_ntfsprogs-2017.3.23 && ./configure --prefix=/usr/local --disable-static && make && make install
